fbc930b6abcbaee91747b2c7da1f35a97ffb99b2
TsionAlemu1 <Tsion.Alemu@relevacelab.com>
2026-05-05 10:36:32 +0300

    add_resourece browesr and loan page whit router

---
+++ share_nest_app/lib/core/theme/app_theme.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import '../constants/app_colors.dart';
+
+class AppTheme {
+  static ThemeData get lightTheme {
+    return ThemeData(
+      scaffoldBackgroundColor: AppColors.background,
+      primaryColor: AppColors.primaryGreen,
+      appBarTheme: const AppBarTheme(
+        backgroundColor: AppColors.primaryGreen,
+        elevation: 0,
+        centerTitle: false,
+        titleTextStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+        iconTheme: IconThemeData(color: Colors.white),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: AppColors.primaryGreen,
+          foregroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(24),
+          ),
+          padding: const EdgeInsets.symmetric(vertical: 16),
+        ),
+      ),
+      inputDecorationTheme: InputDecorationTheme(
+        filled: true,
+        fillColor: AppColors.cardBlue,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide.none,
+        ),
+        hintStyle: const TextStyle(color: AppColors.textGrey),
+      ),
+    );
+  }
+}
